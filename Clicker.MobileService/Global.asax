5b9780548fe19e78ce9ccf00e3a692ba29b7b9b9
Robert Iagar <robert.iagar@gmail.com>
2015-04-23 16:17:17 +0300

    Initial Commit.

---
+++ Clicker.MobileService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clicker.MobileService.WebApiApplication" Language="C#" %>
---
+++ Clicker.MobileService/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace Clicker.MobileService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            WebApiConfig.Register();
+        }
+    }
+}
